0dc57c7f0784c08a6eb4657264d78baded2b935d
Cirill Usatchoff <usatchoff@gmail.com>
2020-02-27 23:38:54 +0200

    Footer year changed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="py-5 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Nextlin 2019</p>
+      <p class="m-0 text-center text-white">Copyright &copy; Nextlin 2020</p>
     </div>
   </footer>
 </template>

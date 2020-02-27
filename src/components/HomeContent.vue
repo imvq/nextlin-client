82fae1c98f46a0cf30fc0d32f0555b34e8a9d861
Cirill Usatchoff <usatchoff@gmail.com>
2020-02-27 23:59:44 +0200

    HomeContent customized

--- src/components/HomeContent.vue
+++ src/components/HomeContent.vue
@@ -1,11 +1,10 @@
 <template>
   <section class="py-5">
     <div class="container">
-      <h1>Section Heading</h1>
-      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      <h1>What we are?</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat
-        architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.
+        If you are a lover of linguistics and are trying to choose a new language to study,
+        our service will help you determine how successful your choice of a language can be, depending on what languages you can speak at the moment and at what level.
       </p>
       <div class="text-center">
         <b-button class="btn-lg" pill variant="primary">Start now</b-button>

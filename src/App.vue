a07e9309ac31cd2fb0c71f683f81c1d356d38d62
Cirill Usatchoff <usatchoff@gmail.com>
2020-02-27 23:35:44 +0200

    Footer inserted to App

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Navbar/>
     <Header/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default {
   name: "App",
   components: {
     Navbar,
-    Header
+    Header,
+    Footer
   }
 };
 </script>

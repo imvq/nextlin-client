9c2c06533edd5d5557693ced699f72678496ed31
Cirill Usatchoff <usatchoff@gmail.com>
2020-02-27 22:28:04 +0200

    Header added

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="py-5 bg-image-full">
+    <img class="img-fluid d-block mx-auto" src="~@/assets/logo.png" alt="Nextlin">
+  </header>
+</template>
+
+<style scoped>
+.main__header {
+  background-image: url("~@/assets/background-min.png");
+}
+</style>
+

1392733f6f507190435a71b0aaff191e52f0e8c4
mavedev <usatchoff@gmail.com>
2020-03-29 00:40:35 +0200

    Work content filled with LangSelectors

---
+++ src/components/WorkContent.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="py-5">
+    <div class="container">
+      <h3>Learn your success index</h3>
+      <p>
+        Choose your native language and languages you know.
+        Also choose your knowleadge levels. If you don't know your
+        exact level, choose 'Middle'.
+      </p>
+      <div class="text-center">
+        <LangSelector />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.wrapper-cenetered {
+  text-align: center;
+}
+</style>
+
+<script>
+import LangSelector from '@/components/LangSelector';
+export default {
+  components: {
+    LangSelector
+  }
+};
+</script>

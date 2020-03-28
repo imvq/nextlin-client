1e80cba912a7a16a5743587439a05a72aa5f284a
mavedev <usatchoff@gmail.com>
2020-03-28 21:43:30 +0200

    Router connected to App

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,11 @@
 <template>
   <div id="app">
-    <Navbar/>
-    <Header/>
-    <HomeContent/>
-    <Footer/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import HomeContent from "@/components/HomeContent";
-
 export default {
-  name: "App",
-  components: {
-    Navbar,
-    Header,
-    Footer,
-    HomeContent
-  }
+  name: "App"
 };
 </script>
-
-<style>
-body {
-  padding-top: 50px;
-}
-</style>
-
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Navbar />
+    <Header />
+    <HomeContent />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import HomeContent from "@/components/HomeContent";
+
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+    Header,
+    Footer,
+    HomeContent
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+body {
+  padding-top: 50px;
+}
+</style>
+

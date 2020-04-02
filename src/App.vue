daa866ac06b488415164d36ae0760ab9d9b21827
mavedev <usatchoff@gmail.com>
2020-04-02 18:36:39 +0300

    Smooth translations added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <template>
   <div id="app">
-    <router-view />
+    <transition
+      name="fade"
+      mode="out-in"
+    >
+      <router-view />
+    </transition>
   </div>
 </template>
+
+<style lang="scss" scoped>
+#app {
+  overflow: hidden;
+  width: 100vw;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+  transform: translateX(0.3em);
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: all .3s ease;
+}
+</style>
 
 <script>
 export default {

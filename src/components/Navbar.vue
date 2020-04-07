c83b9073d04bdb1c9f256ac35be30d1c7de57160
mavedev <usatchoff@gmail.com>
2020-04-07 20:35:01 +0300

    Author link added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,19 +6,28 @@
         to="/"
         href="#"
       >
-        <div class="home-image">
-          <i class="material-icons">home</i>
+        <div class="white-image">
+          <span class="mdi mdi-home" />
         </div>
       </router-link>
+      <a
+        id="link-home"
+        href="https://www.facebook.com/usatchoff"
+      >
+        <div class="white-image">
+          <span class="mdi mdi-facebook" />
+        </div>
+      </a>
     </div>
   </nav>
 </template>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+@import url("https://cdn.materialdesignicons.com/5.0.45/css/materialdesignicons.min.css");
 
-#link-home {
+#link-home, #link-author {
   color: white;
+  font-size: 24px;
 }
 </style>
 

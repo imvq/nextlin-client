4048effc6d6954d53cfe2c6356b5109f2a315173
mavedev <usatchoff@gmail.com>
2020-04-08 21:18:39 +0300

    NotFound component included

---
+++ src/components/NotFoundContent.vue
@@ -0,0 +1,27 @@
+<template>
+  <section
+    id="home-content"
+    class="py-5"
+  >
+    <div class="container">
+      <h1>Page not found :(</h1>
+      <p>
+        We're sorry, we couldn't find the page you requested.
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+#home-content {
+  min-height: calc(100vh - 48px - 296px - 12px);
+}
+
+.wrapper-cenetered {
+  text-align: center;
+}
+</style>
+
+<script>
+export default {};
+</script>
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <h1>Page not found</h1>
-    <p>We're sorry, we couldn't find the page you requested.</p>
+    <NotFoundContent />
   </div>
 </template>
 
 <script>
+import NotFoundContent from '@/components/NotFoundContent';
 
 export default {
-  name: 'NotFound'
+  name: 'NotFound',
+  components: {
+    NotFoundContent
+  }
 };
 </script>
 

1f9169fbe3b7a7b7ff7baf568dcea249211a7f48
mavedev <usatchoff@gmail.com>
2020-04-08 21:14:41 +0300

    404 error implemented

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
@@ -8,9 +7,17 @@ export default new VueRouter({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '/404',
+      component: () => import('../views/NotFound.vue')
+    },
+    {
+      path: '*',
+      redirect: '/404'
+    },
+    {
       path: '/',
       name: 'Home',
-      component: Home,
+      component: () => import('../views/Home.vue'),
       meta: {
         title: 'Nextlin | Home'
       }
---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Page not found</h1>
+    <p>We're sorry, we couldn't find the page you requested.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+body {
+  padding-top: 50px;
+}
+</style>
+
